6f11d4655a920f34d108d2158b76d8934ccf8a57
angel <eirini@learnworlds.com>
2022-08-05 12:29:32 +0300

    More commits

--- learn_dbt/models/example/playing_with_tests.sql
+++ learn_dbt/models/example/playing_with_tests.sql
@@ -1,4 +1,4 @@
 with sample_customer as (
     select * from {{ source('sample_2', 'customer')}}
     )
-select c_custkey,c_mktsegment from sample_customer
+select c_custkey,c_mktsegment, c_acctbal from sample_customer
